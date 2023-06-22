edb8df65298f2617a3058adce1feb8249fdbf546
Russell Oyama <russelloyama@gmail.com>
2023-06-22 09:25:13 -1000

    added react frontend

---
+++ src/dkeeper_assets/src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Footer(){
+    return(
+        <header>
+            <h1><HighlightIcon />Keeper</h1>
+        </header>
+    )
+}
+
+export default Footer;
